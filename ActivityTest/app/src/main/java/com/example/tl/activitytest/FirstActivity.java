b7ebc9e95b58f49d2c624f6e06423394566038d7
lei.tuo <handfourstone@gamil.com>
2018-06-23 21:38:36 +0800

    [4] 使用Menu
    	1. 创建 Menu
    	2. 给 Menu 添加 Item
    	3. 给 Item 绑定 点击事件

--- ActivityTest/app/src/main/java/com/example/tl/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/tl/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.example.tl.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,5 +21,25 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this,"You Clicked Button 1",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this,"You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this,"You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
