0c7eac08c316c196b861f1c48e8c7433bc1ff4b4
lei.tuo <handfourstone@gamil.com>
2018-06-25 23:59:55 +0800

    [21] 实践：随时随地退出应用程序

--- ActivityTest/app/src/main/java/com/example/tl/activitytest/BaseActivity.java
+++ ActivityTest/app/src/main/java/com/example/tl/activitytest/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.tl.activitytest;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,5 +11,12 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("BaseActivity", getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
     }
 }
--- ActivityTest/app/src/main/java/com/example/tl/activitytest/ThirdActivity.java
+++ ActivityTest/app/src/main/java/com/example/tl/activitytest/ThirdActivity.java
@@ -3,6 +3,8 @@ package com.example.tl.activitytest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends BaseActivity {
 
@@ -11,5 +13,12 @@ public class ThirdActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_activity);
         Log.d("tuolei", "ThirdActivity:"+Integer.toString(getTaskId()));
+        Button button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCollector.finishAll();
+            }
+        });
     }
 }
