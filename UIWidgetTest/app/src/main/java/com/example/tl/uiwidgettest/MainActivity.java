7aea19c570db3016c445fa8c555d04cde840a19e
lei.tuo <handfourstone@gamil.com>
2018-06-27 20:46:43 +0800

    [29] UI-Button-通过实现接口的方式注册点击事件监听器

--- UIWidgetTest/app/src/main/java/com/example/tl/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/tl/uiwidgettest/MainActivity.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 在此处添加点击逻辑
-            }
-        });
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                // 在此添加逻辑
+                break;
+            default:
+                break;
+        }
     }
 }
