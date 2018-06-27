9c1d573ea204bb4eb9a2eaec68070216987f4ae4
lei.tuo <handfourstone@gamil.com>
2018-06-27 20:55:58 +0800

    [30] UI-EditText
    	1.属性参考 TextView。
    	2.hint 输入框的提示文本。
    	3.maxLines 最大行数。
    	4.button 的点击事件中获取 EditText 控件的内容。

--- UIWidgetTest/app/src/main/java/com/example/tl/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/tl/uiwidgettest/MainActivity.java
@@ -4,22 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(this);
+        editText = findViewById(R.id.edit_text);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-                // 在此添加逻辑
+                String inputText = editText.getText().toString();
+                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
