1aba56efbe2e638c58485b563ed9af54a27555ef
lei.tuo <handfourstone@gamil.com>
2018-07-05 00:09:21 +0800

    [53] RecyclerView 实现水平滚动

--- RecyclerViewTest/app/src/main/java/com/example/tl/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/tl/recyclerviewtest/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         initFruits();
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
