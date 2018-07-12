904980b63c786b2bba4db4d9be42a6f319aa0c13
lei.tuo <handfourstone@gamil.com>
2018-07-12 21:43:10 +0800

    [66] 根据屏幕特性动态加载布局的技巧
    	==========================================================================
    	屏幕特征	限定符 	描述
    			----------------------------------------------------------
    			small	提供给小屏幕设备的资源
    			----------------------------------------------------------
    	大小		normal	提供给中等屏幕设备的资源
    			----------------------------------------------------------
    			large	提供给大屏幕设备的资源
    			----------------------------------------------------------
    			xlarge	提供给超大屏幕设备的资源
    	--------------------------------------------------------------------------
    			ldpi	提供给低分辨率设备的资源（120dpi以下）
    			----------------------------------------------------------
    			mdpi	提供给中等分辨率设备的资源（120~160dpi）
    			----------------------------------------------------------
    	分辨率		hdpi	提供给高分辨率设备的资源（160~240dpi）
    			----------------------------------------------------------
    			xhdpi	提供给超高分辨率设备的资源（240~320dpi）
    			----------------------------------------------------------
    			xxhdpi	提供给超超高分辨率设备的资源（320~480dpi）
    	--------------------------------------------------------------------------
    			land	提供给横屏设备的资源
    	方向		----------------------------------------------------------
    			port	提供给纵屏设备的资源
    	==========================================================================

--- FragmentTest/app/src/main/java/com/example/tl/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/tl/fragmenttest/MainActivity.java
@@ -20,9 +20,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(this);
-        replaceFragment(new RightFragment());
     }
 
     @Override
