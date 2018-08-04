8dfc9101edba5085de15c7a97e03079a1640affc
handfourstone <handfourstone@gmail.com>
2018-08-04 21:06:02 +0800

    [74] 从 SharedPreferences 中取数据

--- SharedPreferencesTest/app/src/main/java/example/com/sharedpreferencestest/MainActivity.java
+++ SharedPreferencesTest/app/src/main/java/example/com/sharedpreferencestest/MainActivity.java
@@ -3,6 +3,7 @@ package example.com.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age", 26);
                 editor.putBoolean("married", false);
                 editor.apply();
+            }
+        });
+
+        Button restoreData = findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String TAG = "MainActivity";
+                SharedPreferences editor = getSharedPreferences("data", MODE_PRIVATE);
+                String name = editor.getString("name", "");
+                boolean married = editor.getBoolean("married", false);
+                int age = editor.getInt("age",20);
+                Log.d(TAG, "name: " + name);
+                Log.d(TAG, "married: " + married);
+                Log.d(TAG, "age: " + age);
             }
         });
     }
