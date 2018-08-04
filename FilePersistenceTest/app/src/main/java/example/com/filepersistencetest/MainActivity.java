4e340cb5d333e40bf76eb2905fb4b703ba5a0e27
handfourstone <handfourstone@gmail.com>
2018-08-04 16:40:47 +0800

    [70] 将数据存储在文件中

--- FilePersistenceTest/app/src/main/java/example/com/filepersistencetest/MainActivity.java
+++ FilePersistenceTest/app/src/main/java/example/com/filepersistencetest/MainActivity.java
@@ -1,14 +1,53 @@
 
 package example.com.filepersistencetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText  editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edit_text);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = editText.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+            String path = getFilesDir().toString();
+            Log.d("path1",path);
+        }catch (IOException e){
+            e.printStackTrace();
+        } finally{
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
