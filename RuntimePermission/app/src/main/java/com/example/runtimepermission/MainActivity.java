5cf7bc680764d9388e6323c646e6d6d63ba4bd70
handfourstone <handfourstone@gmail.com>
2018-08-12 16:26:19 +0800

    [92] 安卓6.0及以上系统在使用危险权限
    	在使用危险权限时除了要在 AndroidManifest.xml 中申明权限，
    	还要做运行时权限处理，否则会报 perssion denial 的错误。
    
    	在安卓 6.0 以下系统中程序不需要做运行时权限处理程序可以
    	正常运行。

--- RuntimePermission/app/src/main/java/com/example/runtimepermission/MainActivity.java
+++ RuntimePermission/app/src/main/java/com/example/runtimepermission/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.runtimepermission;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button makeCall = (Button)findViewById(R.id.make_call);
+        makeCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:10086"));
+                    startActivity(intent);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
 }
