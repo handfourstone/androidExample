04159dc0500f3af79a8747bbf644b82d3e44080e
handfourstone <handfourstone@gmail.com>
2018-08-22 19:51:51 +0800

    [102] 通知的进阶用法，设置提示音和震动

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -5,11 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,6 +42,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentIntent(pd)
                         .setAutoCancel(true)
+                        //设置提示音
+                        .setSound(Uri.fromFile(new File("/system/media/audio/notifications/Bongo.ogg")))
+                        //设置震动
+                        .setVibrate(new long[]{0, 1000, 1000, 1000})
+                        // 不想单独设置，也可以使用手机默认设置
+                        //.setDefaults(NotificationCompat.DEFAULT_ALL)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .build();
                 //第四步
