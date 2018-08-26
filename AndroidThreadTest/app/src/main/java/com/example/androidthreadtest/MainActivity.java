7bdb8984b27b811a29443f8a869d2fa10a4de3e2
handfourstone <handfourstone@gmail.com>
2018-08-26 20:37:52 +0800

    [122] 在子线程中更新UI报错
    	Only the original thread that created a view hierarchy can touch its views.

--- AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
+++ AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.androidthreadtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        Button changeText = findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:{
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText("Nice to meet you");
+                    }
+                }).start();
+            }
+            default:
+                break;
+        }
     }
 }
