dffbe705d721dbcfe6ec98d591fc05604f285aba
handfourstone <handfourstone@gmail.com>
2018-08-26 17:25:14 +0800

    [118] 使用 JSONObject 解析 JSON 数据

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xmlpull.v1.XmlPullParser;
@@ -59,11 +61,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("http://192.168.2.222/get_data.xml")
+                            .url("http://192.168.2.222/get_data.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    parseXMLWithSAX(responseData);
+                    parseJSONWithJSONObject(responseData);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -133,6 +135,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             xmlReader.setContentHandler(handler);
             xmlReader.parse(new InputSource(new StringReader(xmlData)));
         }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private  void parseJSONWithJSONObject(String jsonData) {
+        try {
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String name = jsonObject.getString("name");
+                String version = jsonObject.getString("version");
+                Log.d("MYJSON", "id is " + id);
+                Log.d("MYJSON", "name is " + name);
+                Log.d("MYJSON", "version is " + version);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
