12c0332539d7ed21fecd52ddf322f3a900ea454b
handfourstone <handfourstone@gmail.com>
2018-08-27 20:22:31 +0800

    [125] 启动和停止服务

--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -3,6 +3,7 @@ package com.example.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
@@ -11,16 +12,21 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MYSERVICE", "onCreate executed");
+        //服务里面任何地方可以通过下面的方式关闭服务
+        //stopSelf();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MYSERVICE", "onStartCommand executed ");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MYSERVICE", "onDestroy executed ");
     }
 
     @Override
