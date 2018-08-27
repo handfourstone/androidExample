099cb153dde03c0f5903731814f3d1fa44425a88
handfourstone <handfourstone@gmail.com>
2018-08-27 21:15:25 +0800

    [127] 使用前台服务

--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,9 +1,13 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
@@ -31,6 +35,16 @@ public class MyService extends Service {
         Log.d("MYSERVICE", "onCreate executed");
         //服务里面任何地方可以通过下面的方式关闭服务
         //stopSelf();
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0, intent, 0 );
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
     }
 
     @Override
