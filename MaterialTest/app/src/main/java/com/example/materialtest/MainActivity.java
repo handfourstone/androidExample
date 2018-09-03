7e38e1d4f88aed5321673f525ac3e92a591cd38f
handfourstone <handfourstone@gmail.com>
2018-09-03 22:15:01 +0800

    [140] 使用 ToolBar
    	1.移除原生的标题栏（ActionBar)
    	2.使用 ToolBar, ToolBar 的外观与 ActionBar 保持一致
    	3.修改标题
    	4.app:showAsAction 的取值
    		a.always 表示永远显示在 ToolBar 中,
    		  如果屏幕空间不够怎不显示；
    		b.ifRomm 表示如果屏幕空间够的话则显示在 ToolBar
    		  中，否则显示在菜单中；
    		c.never 表示永远显示在菜单中。
    	  ToolBar 中的 action 只会显示按钮，菜单中的 action
    	  只会显示文字。

--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.materialtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        // 实现既使用了 ToolBar ，又让它的外观与 ActionBar 一致
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup: {
+                Toast.makeText(this,"You chick backup", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.delete: {
+                Toast.makeText(this,"You chick delete", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.settings: {
+                Toast.makeText(this,"You chick settings", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.settings2:{
+                Toast.makeText(this,"You chick settings2", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            default:
+                break;
+        }
+        return true;
     }
 }
