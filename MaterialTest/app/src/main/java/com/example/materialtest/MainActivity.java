f3303fc480d3669b5a04b2d7b05d4a8064efdb65
handfourstone <handfourstone@gmail.com>
2018-09-04 23:06:57 +0800

    [141] 滑动菜单之 DrawerLayout

--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.materialtest;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:{
+                Toast.makeText(getApplicationContext(),"You chicked menu Button1", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.button2:{
+                Toast.makeText(getApplicationContext(),"You chicked menu Button2", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         // 实现既使用了 ToolBar ，又让它的外观与 ActionBar 一致
         setSupportActionBar(toolbar);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //设置导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
     }
 
     @Override
@@ -43,7 +74,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,"You chick settings2", Toast.LENGTH_SHORT).show();
                 break;
             }
-
+            case android.R.id.home:{ // homeAsUp 按钮的 ID 永远是 android.R.id.home
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            }
             default:
                 break;
         }
